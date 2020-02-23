808584f55cf00bde35254dff49ac5cb9319e0f08
amlagazon <amlagazon@up.edu.ph>
2020-02-23 15:44:44 +0800

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./component/menu/Navbar";
 import Sidebar from "./component/menu/Sidebar";
-import {Grid} from 'semantic-ui-react'
-import './App.css';
+import { Grid } from "semantic-ui-react";
+import { useMediaQuery } from "react-responsive";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    <Grid>
-      <Grid.Row columns={1} only="mobile">
-        <Grid.Column>
-          <Sidebar/>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-    <Grid>
-      <Grid.Row columns={1} only="desktop">
-        <Grid.Column>
-          <Navbar/>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-    </div>
-  );
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)"
+  });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: "(max-device-width: 1224px)"
+  });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  if (isPortrait || isTabletOrMobileDevice) {
+    return (
+      <Grid>
+        <Grid.Row columns={1} only="mobile">
+          <Grid.Column>
+            <Sidebar />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  } else if (isDesktopOrLaptop || isTabletOrMobile) {
+    return (
+      <Grid>
+        <Grid.Row columns={1}>
+          <Grid.Column>
+            <Navbar />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
 
 export default App;
